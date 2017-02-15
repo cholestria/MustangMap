48439a934608451537bfe74075dbb00304a55413
Olivia Knott <cholestria@gmail.com>
2017-02-15 21:12:06 +0000

    both charts displaying on main page

--- static/charts.js
+++ static/charts.js
@@ -103,15 +103,6 @@ function makePopulationChart(data_endpoint, div_id) {
             tooltip: {
                 shared: false
             },
-            legend: {
-                // layout: 'vertical',
-                // align: 'left',
-                // x: 120,
-                // verticalAlign: 'top',
-                // y: 100,
-                // floating: true,
-                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-            },
             navigation: {
               buttonOptions: {
                 enabled: false
@@ -156,4 +147,114 @@ function makePopulationChart(data_endpoint, div_id) {
     });
 }
 
+function makeAdoptionChart(data_endpoint, div_id) {
+    // Make a AJAX call to get the json data for the state
+    $.get(data_endpoint, function(data) {
 
+        var years = [];
+        var pop_years = [];
+        var year_object;
+        var footnote_object;
+        var population_object;
+        var footnote_list;
+        var result;
+        var state_name;
+        var horses_adopted = [];
+        var burros_adopted = [];
+        var horses_removed = [];
+        var burros_removed = [];
+        var horse_population = [];
+        var burro_population = [];
+
+        result=data;
+        state_name=result.Name;
+        year_object=result.StateData;
+        years=Object.keys(year_object);
+        footnote_object=result.Footnotes;
+        footnote_list=Object.keys(footnote_object);
+        population_object=result.PopData;
+        pop_years=Object.keys(population_object);
+
+        for (var i=0; i < years.length; i++) {
+            var year = years[i];
+            horses_adopted.push(year_object[year][0]);
+            burros_adopted.push(year_object[year][1]);
+            horses_removed.push(year_object[year][2]);
+            burros_removed.push(year_object[year][3]);
+        }
+
+        var header = "Adoptions and Removals in " + state_name + " Over Time";
+
+        Highcharts.setOptions({
+            lang: {
+                thousandsSep: ','
+            }
+        });
+
+        Highcharts.chart(div_id, {
+            chart: {
+                type: 'areaspline'
+            },
+            title: {
+                text: header
+            },
+            subtitle: {
+                text: "Source: BLM"
+            },
+            legend: {
+                // layout: 'vertical',
+                // align: 'left',
+                // verticalAlign: 'top',
+                // // x: 400,
+                // y: 50,
+                // floating: true,
+                borderWidth: 0,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            xAxis: {
+                categories: years,
+            },
+            yAxis: {
+                title: {
+                    text: '# of horses or burros'
+                }
+            },
+            tooltip: {
+                shared: false,
+                valueSuffix: ' '
+            },
+            navigation: {
+              buttonOptions: {
+                enabled: false
+              }
+            },
+            credits: {
+                enabled: true,
+                text: footnote_list,
+                href: 'https://www.blm.gov/wo/st/en/prog/whbprogram/herd_management/Data.html',
+                position: {
+                    align: 'left',
+                    x: 20
+                }
+            },
+            plotOptions: {
+                areaspline: {
+                    fillOpacity: 0.5
+                }
+            },
+            series: [{
+                name: 'Horses Adopted',
+                data: horses_adopted
+            }, {
+                name: 'Burros Adopted',
+                data: burros_adopted
+            }, {
+                name: 'Horses Removed',
+                data: horses_removed
+            }, {
+                name: 'Burros Removed',
+                data: burros_removed
+            }]
+        });
+     });
+}
