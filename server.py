c5384557181eb1001d6fae155ecfbb820c9763da
Olivia Knott <cholestria@gmail.com>
2017-02-24 22:11:31 +0000

    added a text information div to the map page

--- server.py
+++ server.py
@@ -143,7 +143,6 @@ def upload_file():
         return upload()
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        print filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         name=request.form.get("name")
